b3d43eef17d412ba3f613fbdba75f689616f556e
SaidHoja <saidhojabusiness@gmail.com>
2023-03-25 23:39:39 -0400

    working dynamic form cuz im soooo good at coding and super cool

--- pages/creditHistoryForm.js
+++ pages/creditHistoryForm.js
@@ -1,36 +1,69 @@
-import { Input,InputGroup, InputLeftElement, Stack } from '@chakra-ui/react'
+import { Input,InputGroup, InputLeftElement, Stack, IconButton, HStack } from '@chakra-ui/react'
+import { AddIcon} from '@chakra-ui/icons'
 import { Center, Square, Circle } from '@chakra-ui/react'
 import React, { useState } from 'react'
 
 export default function creditHistoryInputter(){
     //const [creditAccount, setCreditAccounts] = useState([]) 
-    const [startDate, setStartDate] = useState(new Date());
+    const [creditDates, setCreditDates] = useState([
+        {startDate: "", endDate: ""},{startDate: "", endDate: ""}
+    ]);
+    function AddRow(){
+        console.log("button pressed")
+        let newCreditDate =  {startDate: "", endDate: ""}
+        setCreditDates([...creditDates,newCreditDate])
+    }
+    function HandleChange(event,index){
+        let data = [...creditDates];
+        data[index][event.target.name] = event.target.value;
+        setCreditDates(data)
+    }
+    console.log(creditDates)
     return (
         <>
         <Center >
             <Stack spacing = {3} colorscheme="messenger">
-                <InputGroup>
-                <InputLeftElement 
-                    children="Credit Account 1"
-                />
-                <Input
-                    placeholder="Select Date and Time"
-                    size="md"
-                    type = "month"
-                 />
-                </InputGroup>
-                <Input 
-                 placeholder="Select Date and Time"
-                 size="md"
-                 type = "month"
-                />
-                <Input 
-                 placeholder="Select Date and Time"
-                 size="md"
-                 type = "month"
-                />
+                {
+                 creditDates.map( (input,index) => {
+                    return (
+                        <InputGroup key = {index}>
+                            <InputLeftElement children = {index + 1} />
+                            <Input
+                                name = 'startDate'
+                                size="md"
+                                type = "month"
+                                value={input.startDate}
+                                onChange = {event => HandleChange(event,index)}
+                            />
+                            <Input
+                                name = 'endDate'
+                                size="md"
+                                type = "month"
+                                value={input.endDate}
+                                onChange = {event => HandleChange(event,index)}
+                            />
+                    </InputGroup>
+                    )
+                }
+                
+                )}
+                 <IconButton onClick={AddRow} icon={<AddIcon />}/>
             </Stack>
         </Center>
         </>
+        
     )
 }
+/*
+                <InputGroup>
+                    <InputLeftElement 
+                        children="1"
+                    />
+                    <Input
+                        placeholder="Select Date and Time"
+                        size="md"
+                        type = "month"
+                    />
+                 </InputGroup>
+                 <IconButton onClick="AddRow" icon={<AddIcon />} />
+*/
